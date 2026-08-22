33db4eb62544284278844b577ec6e890a3ad5678
ademnazif <ademnazifciloglu@gmail.com>
2026-08-22 20:59:31 +0300

    Update Program.cs

--- Spor Porjesi/Program.cs
+++ Spor Porjesi/Program.cs
@@ -1,18 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); // IIS ile çakışmayı önlemek için devre dışı bırakıldı
 app.UseStaticFiles();
 
 app.UseRouting();
